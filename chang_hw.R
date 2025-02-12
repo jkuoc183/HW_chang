219e8aed39e987c91b2ab68f85d8617e5e1441b9
jkuoc183 <jackkuochang.com>
2025-02-11 22:21:27 -0500

    Print the mean and SD

--- chang_hw.R
+++ chang_hw.R
@@ -1,3 +1,4 @@
 random_number <- rnorm(100, mean = 25, sd =1)
 mean_value <- mean(random_number)
 sd_value <- sd(random_number)
+print(paste("The Mean is", mean_value, "and the Standard Deviation is", sd_value))
